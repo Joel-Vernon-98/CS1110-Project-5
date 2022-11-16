e71190499f523a413ebfd8bb0168b10494051f8a
Joel Vernon <joel.l.vernon@wmich.edu>
2022-11-16 13:59:18 -0500

    Changed input validation to use regular expressions

--- p5_module.py
+++ p5_module.py
@@ -1,48 +1,40 @@
+import re
 # Constants
-VALID_OPERATIONS = ("|", "&", "^", "q")
-VALID_HEX_DIGITS = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F")
-HEX_VALUES = {"0": "0000", "1": "0001", "2": "0010", "3": "0011",
-              "4": "0100", "5": "0101", "6": "0110", "7": "0111",
-              "8": "1000", "9": "1001", "A": "1010", "B": "1011",
-              "C": "1100", "D": "1101", "E": "1110", "F": "1111",
-              }
+HEX_VALUES = ["0000", "0001", "0010", "0011",
+              "0100", "0101", "0110", "0111",
+              "1000", "1001", "1010", "1011",
+              "1100", "1101", "1110", "1111",
+              ]
 
 
 def is_valid_hex_string(hex_string: str) -> bool:
     text = "Please enter an 8-digit hexadecimal integer"
-    if len(hex_string) > 8:
-        print(text)
-        return False
-    for digit in hex_string:
-        if digit in VALID_HEX_DIGITS:
-            continue
-        print(text)
-        return False
-    return True
+    if len(hex_string) <= 8 and re.search("^[a-fA-F0-9]*$", hex_string):
+        return True
+    print(text)
+    return False
 
 
 def get_operation() -> str:
     text = "Please enter |, &, ^, or q"
     while True:
         operation = input("Enter operation: ")
-        if operation in VALID_OPERATIONS:
+        if re.search("^[|^&q]$", operation):
             return operation
         print(text)
 
 
 def get_integer_count() -> int:
+    text = "Error: Please enter an integer value greater than 1."
     while True:
         integer_count = input("Enter number of integers: ")
-        try:
-            integer_count = int(integer_count)
-            if integer_count < 2:
-                raise ValueError
-            return integer_count
-        except ValueError:
-            print(f"Error: Please enter an integer value greater than 1.")
+        regex_string = "[2-9][0-9]*$|^[1-9][0-9]+$"
+        if re.search(regex_string, integer_count):
+            return int(integer_count)
+        print(text)
 
 
-def hex_operation(integer_list: list[str], operator: str, step=0) -> list[str]:
+def hex_operation(integer_list, operator: str, step=0):
     result = hex(eval(f"{integer_list[step]} {operator} {integer_list[-1]}"))
     if step == 0:
         integer_list.append(result)
@@ -52,14 +44,14 @@ def hex_operation(integer_list: list[str], operator: str, step=0) -> list[str]:
     return hex_operation(integer_list, operator, step + 1)
 
 
-def binary_operation(integer_list: list[str]) -> list[str]:
+def binary_operation(integer_list):
     result = []
     for hex_integer in integer_list:
         binary_string = ""
-        hex_integer = hex_integer.removeprefix("0x")
+        hex_integer = hex_integer.lstrip("0x")
         hex_digits = string_split(hex_integer, 1)
         for hex_digit in hex_digits:
-            binary_string += HEX_VALUES[hex_digit.upper()]
+            binary_string += HEX_VALUES[int(eval(f"0x{hex_digit}"))]
         result.append(binary_string)
     return result
 
@@ -74,16 +66,16 @@ def string_split(input_string, split_length):
 
 
 def hex_formatter(hex_string: str) -> str:
-    return hex_string.removeprefix("0x").zfill(8).upper()
+    return hex_string.lstrip("0x").zfill(8).upper()
 
 
 def binary_formatter(binary_string: str) -> str:
-    binary_string = binary_string.removeprefix("0b").zfill(32)
+    binary_string = binary_string.lstrip("0b").zfill(32)
     byte_list = string_split(binary_string, 8)
     return "  ".join(byte_list)
 
 
-def print_results(hex_integers: list[str], binary_integers: list[str], operator):
+def print_results(hex_integers, binary_integers, operator):
     print("Hexadecimal operation: ")
     i = 0
     while i < len(hex_integers):
